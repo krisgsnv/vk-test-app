a08a378b44e9c66ff256023932ff1521cefee363
krisgsnv <krigasf@gmail.com>
2024-03-14 13:49:38 +0300

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { View, SplitLayout, SplitCol } from '@vkontakte/vkui';
+import { useActiveVkuiLocation } from '@vkontakte/vk-mini-apps-router';
+
+import { Persik, Home } from './panels';
+import { DEFAULT_VIEW_PANELS } from './routes';
+
+export const App = () => {
+  const { panel: activePanel = DEFAULT_VIEW_PANELS.HOME } = useActiveVkuiLocation();
+
+  return (
+    <SplitLayout>
+      <SplitCol>
+        <View activePanel={activePanel}>
+          <Home id="home" />
+          <Persik id="persik" />
+        </View>
+      </SplitCol>
+    </SplitLayout>
+  );
+};
